2c05351d055be8cca009f3bb7d76c297e5c70c24
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-05-16 02:06:33 -0500

    Expose Playwright native libraries in Nix runtime

--- dev.nix
+++ dev.nix
@@ -1,5 +1,39 @@
 { pkgs, ... }:
 
+let
+  playwrightLibs = with pkgs; [
+    glib
+    nss
+    nspr
+    atk
+    at-spi2-atk
+    at-spi2-core
+    cups
+    libdrm
+    dbus
+    expat
+    xorg.libxcb
+    xorg.libX11
+    xorg.libXcomposite
+    xorg.libXdamage
+    xorg.libXext
+    xorg.libXfixes
+    xorg.libXrandr
+    xorg.libXi
+    xorg.libXtst
+    xorg.libxshmfence
+    libxkbcommon
+    mesa
+    pango
+    cairo
+    alsa-lib
+    fontconfig
+    freetype
+    gtk3
+    gdk-pixbuf
+    stdenv.cc.cc.lib
+  ];
+in
 {
   packages = with pkgs; [
     nodejs_20
@@ -9,9 +43,11 @@
     nspr
     atk
     at-spi2-atk
+    at-spi2-core
     cups
     libdrm
     dbus
+    expat
     xorg.libxcb
     xorg.libX11
     xorg.libXcomposite
@@ -19,14 +55,22 @@
     xorg.libXext
     xorg.libXfixes
     xorg.libXrandr
+    xorg.libXi
+    xorg.libXtst
+    xorg.libxshmfence
     libxkbcommon
     mesa
     pango
     cairo
     alsa-lib
+    fontconfig
+    freetype
+    gtk3
+    gdk-pixbuf
   ];
 
   env = {
     PLAYWRIGHT_BROWSERS_PATH = "$HOME/.cache/ms-playwright";
+    LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath playwrightLibs}";
   };
 }
