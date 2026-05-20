436701eb4cec2d7e287352305ef6a95d2297698a
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-05-19 20:17:36 -0500

    Export queued narrator callable

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,11 @@
 export {
   generateStorySession,
   generateWeeklyStoryScroll,
-  generateNarratorScript,
   generateEmotionalArcSummary,
   createPublicStoryShare,
   prepareVoiceoverJob,
   refreshStoryTimeline,
   rebuildUserStoryArchive
 } from "./storytime.js";
+
+export { generateNarratorScript } from "./generate-narrator-script.js";
