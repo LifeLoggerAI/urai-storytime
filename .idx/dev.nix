1b0b29781e900f81017fb1a183854c45fdb2abcc
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-05-21 14:10:11 -0500

    Add Playwright runtime libraries to IDX env

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,8 +1,79 @@
-{pkgs}: {
-  channel = "stable-24.05";
-  packages = [
-    pkgs.nodejs_20
+{pkgs}:
+
+let
+  playwrightLibs = with pkgs; [
+    glib
+    nss
+    nspr
+    atk
+    at-spi2-atk
+    at-spi2-core
+    cups
+    libdrm
+    dbus
+    expat
+    xorg.libxcb
+    xorg.libX11
+    xorg.libXcomposite
+    xorg.libXdamage
+    xorg.libXext
+    xorg.libXfixes
+    xorg.libXrandr
+    xorg.libXi
+    xorg.libXtst
+    xorg.libxshmfence
+    libxkbcommon
+    mesa
+    pango
+    cairo
+    alsa-lib
+    fontconfig
+    freetype
+    gtk3
+    gdk-pixbuf
+    stdenv.cc.cc.lib
   ];
+in
+{
+  channel = "stable-24.05";
+  packages = with pkgs; [
+    nodejs_20
+    chromium
+    glib
+    nss
+    nspr
+    atk
+    at-spi2-atk
+    at-spi2-core
+    cups
+    libdrm
+    dbus
+    expat
+    xorg.libxcb
+    xorg.libX11
+    xorg.libXcomposite
+    xorg.libXdamage
+    xorg.libXext
+    xorg.libXfixes
+    xorg.libXrandr
+    xorg.libXi
+    xorg.libXtst
+    xorg.libxshmfence
+    libxkbcommon
+    mesa
+    pango
+    cairo
+    alsa-lib
+    fontconfig
+    freetype
+    gtk3
+    gdk-pixbuf
+  ];
+  env = {
+    PLAYWRIGHT_BROWSERS_PATH = "$HOME/.cache/ms-playwright";
+    PLAYWRIGHT_CHROMIUM_EXECUTABLE_PATH = "${pkgs.chromium}/bin/chromium";
+    LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath playwrightLibs}";
+  };
   idx.extensions = [
     
   ];
