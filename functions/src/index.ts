dc2aaff4b68ae96b693f10b65d65eec26a1d8302
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-06-28 18:30:20 -0500

    Export Storytime public share revoke flow

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,3 +9,4 @@ export {
 export { generateNarratorScript } from "./generate-narrator-script.js";
 export { generateEmotionalArcSummary } from "./generate-emotional-arc-summary.js";
 export { generateWeeklyStoryScroll } from "./generate-weekly-story-scroll.js";
+export { revokePublicStoryShare } from "./revoke-public-story-share.js";
