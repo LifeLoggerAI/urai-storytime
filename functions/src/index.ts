e3334915a8623e27d25dfb6c39e1464943033934
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-09-23 23:01:07 -0500

    Functions: export real timeline and archive callables

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,14 +1,14 @@
 export {
   generateStorySession,
-  prepareVoiceoverJob,
-  refreshStoryTimeline,
-  rebuildUserStoryArchive
+  prepareVoiceoverJob
 } from "./storytime.js";
 
 export { createPublicStoryShare, revokePublicStoryShare } from "./public-story-share-lifecycle.js";
 export { generateNarratorScript } from "./generate-narrator-script.js";
 export { generateEmotionalArcSummary } from "./generate-emotional-arc-summary.js";
 export { generateWeeklyStoryScroll } from "./generate-weekly-story-scroll.js";
+export { refreshStoryTimeline } from "./refresh-story-timeline.js";
+export { rebuildUserStoryArchive } from "./rebuild-user-story-archive.js";
 export {
   upsertFiniteTimeCanonRegistry,
   upsertFiniteTimeShotGraph,
