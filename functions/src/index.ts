697aeef1fde367f2d07b1f73ecc81eb924a3ae28
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-09-24 00:06:21 -0500

    Drafts: export private draft callables

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -17,3 +17,5 @@ export {
 export { health, readiness } from "./readiness.js";
 
 export { requestPrivacyOperation } from "./privacy-requests.js";
+
+export { saveStoryDraft, deleteStoryDraft } from "./story-drafts.js";
